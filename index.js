8cbc3079df3ea02518bb47e53d48d99773a7c21a
Artur Fidelis <65771411+arturfidelis-avoubiclabs@users.noreply.github.com>
2020-10-09 21:26:57 -0300

    insercao do projeto

---
+++ index.js
@@ -0,0 +1,58 @@
+// função para quando for acessar pelo campo USUARIO
+function usuario(){
+
+  // Recebendo os dados do HTML, através dos IDs
+  var estab = document.getElementById("estab")
+  var adm = document.getElementById("adm")
+  var entrar = document.getElementById("entrar")
+  var esqueciSenha = document.getElementById("esqueciSenha")
+  var criarConta = document.getElementById("criarConta")
+
+  // Retornando ao HTML os dados feitos com o JS
+  estab.innerHTML = "<input placeholder='E-mail:'></input>"
+  adm.innerHTML = "<input type='password' placeholder='Senha:'></input>"
+  entrar.innerHTML = "<button> ENTRAR </button>"
+  esqueciSenha.innerHTML = "Esqueci minha senha:"
+  criarConta.innerHTML = "Criar Conta:"
+
+}
+
+// função para quando for acessar pelo campo ESTABELECIMENTO
+function estabelecimento(){
+
+  // Recebendo os dados do HTML, através dos IDs
+  var usu = document.getElementById("usu")
+  var adm = document.getElementById("adm")
+  var entrar = document.getElementById("entrar")
+  var senha = document.getElementById("senha_estab_email_adm")
+  var esqueciSenha = document.getElementById("esqueciSenha")
+  
+  usu.innerHTML = ""
+  
+  // Retornando ao HTML os dados feitos com o JS
+  adm.innerHTML = "<input type=text placeholder='E-mail:'></input>"
+  senha.innerHTML = "<input type='password' placeholder='Senha:'></input>"
+  entrar.innerHTML = "<button> ENTRAR </button>"
+  esqueciSenha.innerHTML = "Esqueci minha senha:"
+
+}
+
+// função para quando for acessar pelo campo ADM
+function adm(){
+
+  // Recebendo os dados do HTML, através dos IDs
+  var usu = document.getElementById("usu")
+  var estab = document.getElementById("estab")
+  var entrar = document.getElementById("entrar")
+  var email = document.getElementById("senha_estab_email_adm")
+  var senha = document.getElementById("senha_adm")
+  
+  usu.innerHTML = "" 
+  estab.innerHTML = ""
+
+  // Retornando ao HTML os dados feitos com o JS
+  email.innerHTML = "<input placeholder='E-mail:'></input>"
+  senha.innerHTML = "<input type='password' placeholder='Senha:'></input>"
+  entrar.innerHTML = "<button onclick=''> ENTRAR </button>"
+
+}
